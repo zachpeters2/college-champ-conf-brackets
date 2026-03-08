ce1f6866db22db4c3f10dd136a16894175c408cf
zachpeters2 <zach.peters.2@gmail.com>
2026-03-08 16:21:45 -0700

    Add CONFERENCE_MAP configuration for conferences

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFERENCE_MAP = [
+    { id: 50, name: "All D1 Games" },
+    { id: 44, name: "Mountain West" },
+    { id: 8,  name: "Big 12" },
+    { id: 2,  name: "ACC" },
+    { id: 7,  name: "Big Ten" },
+    { id: 1,  name: "SEC" },
+    { id: 62, name: "Big East" },
+    { id: 9,  name: "Pac-12" }
+];
+
+export default CONFERENCE_MAP;
